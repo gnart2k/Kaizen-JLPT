ec422c89bff6d14553e8d0c9226ab5076e0a71e8
trangbg2000 <trangbg2000@gmail.com>
2025-12-06 00:25:40 +0700

    Enhance: Allow fetching difficulty levels by language in API and hook

--- src/app/api/difficulty-levels/route.ts
+++ src/app/api/difficulty-levels/route.ts
@@ -1,11 +1,19 @@
 import { db } from '@/lib/db';
-import { NextResponse } from 'next/server';
+import { difficultyLevels } from '@/lib/db/schema';
+import { eq } from 'drizzle-orm';
+import { NextRequest, NextResponse } from 'next/server';
 
-export async function GET() {
+export async function GET(request: NextRequest) {
   try {
-    const difficultyLevels = await db.query.difficultyLevels.findMany();
-    console.log(difficultyLevels)
-    return NextResponse.json(difficultyLevels);
+    const { searchParams } = new URL(request.url);
+    const language = searchParams.get('language');
+
+    const difficultyLevelsQuery = db.query.difficultyLevels.findMany({
+      where: language ? eq(difficultyLevels.language, language) : undefined,
+    });
+
+    const levels = await difficultyLevelsQuery;
+    return NextResponse.json(levels);
   } catch (error) {
     console.error('GET difficulty levels error:', error);
     return NextResponse.json({ message: 'Internal Server Error' }, { status: 500 });
